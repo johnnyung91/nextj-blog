aa654d0b0be278181e5aad3fbb7e218c401959ed
Johnny Ung <johnnyung91@gmail.com>
2020-11-06 02:35:45 -0800

    added title metadata to head of first post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -5,6 +5,10 @@ import Head from 'next/head'
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">
