2d6101cb67a1e967bbe7c633fb1831ca6d02f15f
Johnny Ung <johnnyung91@gmail.com>
2020-11-10 13:24:13 -0800

    added title to head in [id]; created Date component

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,13 @@
 import Layout from "../../components/layout";
+import Head from 'next/head'
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
